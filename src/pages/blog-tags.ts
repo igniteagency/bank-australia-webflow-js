c6eea18b3aad4d65dfbdc65f49ef31bad2121121
Paras Shah <parasshah195@gmail.com>
2024-03-26 20:44:48 +0530

    Add tags page script

--- src/pages/blog-tags.ts
+++ src/pages/blog-tags.ts
@@ -1,7 +1,5 @@
 import { initDuplicateArticlesRemoval } from 'src/components/duplicates-remove';
-import { initArticlesSlider } from 'src/components/slider';
 
 window.Webflow?.push(() => {
   initDuplicateArticlesRemoval();
-  initArticlesSlider();
 });
