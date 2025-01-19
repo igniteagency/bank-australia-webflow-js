cf1d66e2b316e1b09297b4a140bfb7f08a65136c
Paras Shah <parasshah195@gmail.com>
2025-01-19 12:37:23 +0530

    Update autoplay slider
    Options, classnames, properties, etc.
    Removed modules because they're already included in the CDN bundle

--- dist/prod/components/autoplay-slider.js
+++ dist/prod/components/autoplay-slider.js
@@ -5,22 +5,29 @@ if (window.SCRIPTS_ENV === 'dev') {
 } else {
   (() => {
   // src/components/autoplay-slider.ts
-  var _a;
-  (_a = window.Webflow) == null ? void 0 : _a.push(() => {
-    if (!window.EXECUTED_SCRIPT.includes("autoplay-slider")) {
+  window.Webflow = window.Webflow || [];
+  window.Webflow.push(() => {
+    if (window.EXECUTED_SCRIPT.includes("autoplay-slider")) {
+      console.debug("Autoplay slider script already executed");
       return;
     }
+    let SLIDER_GAP = 80;
     const SECTION_SELECTOR = '[data-blog-slider-el="section"]';
     const NAV_PREV_BUTTON_SELECTOR = '[data-blog-slider-el="nav-prev"]';
     const NAV_NEXT_BUTTON_SELECTOR = '[data-blog-slider-el="nav-next"]';
     const AUTOPLAY_BUTTON_SELECTOR = '[data-blog-slider-el="autoplay"]';
     const PAGINATION_SELECTOR = '[data-blog-slider-el="pagination"]';
+    const SLIDER_GAP_OVERRIDE_ATTR = "data-blog-slider-gap";
     document.querySelectorAll(SECTION_SELECTOR).forEach((sliderSectionEl) => {
       const swiperEl = sliderSectionEl.querySelector(".swiper");
       const navPrevButtonEl = sliderSectionEl.querySelector(NAV_PREV_BUTTON_SELECTOR);
       const navNextButtonEl = sliderSectionEl.querySelector(NAV_NEXT_BUTTON_SELECTOR);
       const autoplayButtonEl = sliderSectionEl.querySelector(AUTOPLAY_BUTTON_SELECTOR);
       const paginationEl = sliderSectionEl.querySelector(PAGINATION_SELECTOR);
+      const sliderGapOverrideEl = sliderSectionEl.querySelector(`[${SLIDER_GAP_OVERRIDE_ATTR}]`);
+      if (sliderGapOverrideEl) {
+        SLIDER_GAP = parseInt(sliderGapOverrideEl.getAttribute(SLIDER_GAP_OVERRIDE_ATTR) || "80");
+      }
       if (!swiperEl) {
         console.debug("No swiper element found in the slider section", sliderSectionEl);
         return;
@@ -40,7 +47,7 @@ if (window.SCRIPTS_ENV === 'dev') {
         el: paginationEl,
         clickable: true,
         bulletElement: "button",
-        bulletClass: "reviews-slider_nav-item",
+        bulletClass: "autoplay-slider_nav-item",
         bulletActiveClass: "is-active",
         renderBullet: (index, className) => {
           return `<button class="${className}" type="button">
@@ -48,52 +55,59 @@ if (window.SCRIPTS_ENV === 'dev') {
         </button>`;
         }
       } : false;
-      const swiper = new Swiper(swiperEl, {
-        modules: [Navigation, Autoplay, A11y, Pagination],
-        loop: false,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 40,
-        navigation: navigationConfig,
-        pagination: paginationConfig,
-        autoplay: {
-          delay: 5e3,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true
-        },
-        a11y: {
-          enabled: true
-        },
-        breakpoints: {
-          481: {
-            slidesPerView: 2
+      try {
+        const swiper = new Swiper(swiperEl, {
+          loop: true,
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: SLIDER_GAP,
+          centeredSlides: true,
+          navigation: navigationConfig,
+          pagination: paginationConfig,
+          slideActiveClass: "is-active",
+          slidePrevClass: "is-previous",
+          slideNextClass: "is-next",
+          autoplay: {
+            delay: 5e3,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
           },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 20
+          observer: true,
+          observeParents: true,
+          a11y: {
+            enabled: true
+          },
+          breakpoints: {
+            480: {
+              spaceBetween: SLIDER_GAP / 2
+            },
+            768: {
+              spaceBetween: SLIDER_GAP / 1.5
+            }
           }
+        });
+        console.debug("Swiper initialized", swiper);
+        if (autoplayButtonEl) {
+          let isPlaying = true;
+          updateAutoplayButtonState(autoplayButtonEl, isPlaying);
+          autoplayButtonEl.addEventListener("click", () => {
+            if (isPlaying) {
+              swiper.autoplay.stop();
+            } else {
+              swiper.autoplay.start();
+            }
+          });
+          swiper.on("autoplayStart", () => {
+            isPlaying = true;
+            updateAutoplayButtonState(autoplayButtonEl, isPlaying);
+          });
+          swiper.on("autoplayStop", () => {
+            isPlaying = false;
+            updateAutoplayButtonState(autoplayButtonEl, isPlaying);
+          });
         }
-      });
-      if (autoplayButtonEl) {
-        let isPlaying = true;
-        updateAutoplayButtonState(autoplayButtonEl, isPlaying);
-        autoplayButtonEl.addEventListener("click", () => {
-          isPlaying = !isPlaying;
-          if (isPlaying) {
-            swiper.autoplay.start();
-          } else {
-            swiper.autoplay.pause();
-          }
-          updateAutoplayButtonState(autoplayButtonEl, isPlaying);
-        });
-        swiper.on("autoplayStart", () => {
-          isPlaying = true;
-          updateAutoplayButtonState(autoplayButtonEl, isPlaying);
-        });
-        swiper.on("autoplayStop", () => {
-          isPlaying = false;
-          updateAutoplayButtonState(autoplayButtonEl, isPlaying);
-        });
+      } catch (error) {
+        console.error("Error initializing Swiper", error);
       }
     });
     function updateAutoplayButtonState(button, isPlaying) {
