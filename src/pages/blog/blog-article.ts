ac02dda8e97bd2424bf581864367068a79f6b506
Paras Shah <parasshah195@gmail.com>
2025-04-29 15:14:39 +0530

    Remove article gallery slider; port social share links script

--- src/pages/blog/blog-article.ts
+++ src/pages/blog/blog-article.ts
@@ -1,43 +1,12 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-
 window.Webflow?.push(() => {
-  initGallerySlider();
+  setSocialShareLinks();
 });
 
-function initGallerySlider() {
-  const SECTION_SELECTOR = '[data-gallery-layout="Image Slider"]';
-  const NAV_PREV_BUTTON_SELECTOR = '[data-gallery-slider-el="nav-prev"]';
-  const NAV_NEXT_BUTTON_SELECTOR = '[data-gallery-slider-el="nav-next"]';
+function setSocialShareLinks() {
+  const socialLinks = document.querySelectorAll('.social-share-icon');
+  const pageUrl = window.location.href;
 
-  document.querySelectorAll(SECTION_SELECTOR).forEach((sliderSectionEl) => {
-    const swiperEl = sliderSectionEl.querySelector('.swiper');
-    const navPrevButtonEl = sliderSectionEl.querySelector(NAV_PREV_BUTTON_SELECTOR);
-    const navNextButtonEl = sliderSectionEl.querySelector(NAV_NEXT_BUTTON_SELECTOR);
-
-    if (!swiperEl) {
-      console.debug('No swiper element found in the slider section', sliderSectionEl);
-      return;
-    }
-
-    const slidesCount = swiperEl.querySelectorAll('.swiper-slide').length;
-
-    if (!slidesCount) {
-      console.debug('No slides found in the slider. Deleting the section', swiperEl);
-      sliderSectionEl.remove();
-      return;
-    }
-
-    new Swiper(swiperEl, {
-      modules: [Navigation],
-      loop: false,
-      spaceBetween: 20,
-      slidesPerView: 'auto',
-      navigation: {
-        nextEl: navNextButtonEl,
-        prevEl: navPrevButtonEl,
-        disabledClass: 'is-disabled',
-      },
-    });
+  socialLinks.forEach((socialLink) => {
+    socialLink.setAttribute('href', socialLink.dataset.url + pageUrl);
   });
 }
