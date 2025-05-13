adf01442884b78b247b93957304d2ef0a71ddcfb
Paras Shah <parasshah195@gmail.com>
2025-05-13 23:26:46 +0530

    Move Webflow scroll disable code to global file

--- src/global.ts
+++ src/global.ts
@@ -4,14 +4,22 @@ import { LazyLoadVideoEmbeds } from '$utils/lazy-load-videos';
 
 import { setImageCardAriaLabel } from './components/image-card';
 
-gsap.registerPlugin(ScrollTrigger);
-
 document.addEventListener('DOMContentLoaded', () => {
+  gsap.registerPlugin(ScrollTrigger);
   new LazyLoadVideoEmbeds().init();
 });
 
 window.Webflow = window.Webflow || [];
 window.Webflow.push(() => {
+  disableWebflowScroll();
   setImageCardAriaLabel();
   initBugHerd();
 });
+
+/**
+ * remove Webflow script-based smooth scroll
+ * in favour of CSS-native smooth scroll on `html`
+ */
+function disableWebflowScroll() {
+  jQuery(document).off('click.wf-scroll');
+}
--- src/utils/disable-webflow-scroll.ts
+++
@@ -1,4 +0,0 @@
-// Added directly to the site in Global code body area
-window.Webflow?.push(() => {
-  jQuery?.(document).off('click.wf-scroll');
-});
