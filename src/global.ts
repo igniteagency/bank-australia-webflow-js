74cf010d9e9be9872b3ee9507a424190ca2d16e6
Paras Shah <parasshah195@gmail.com>
2025-05-13 22:58:05 +0530

    Lazy load Webflow video element embeds

--- src/global.ts
+++ src/global.ts
@@ -1,9 +1,17 @@
 import '$utils/alpine-webflow';
 import { initBugHerd } from '$utils/bugherd-script';
+import { LazyLoadVideoEmbeds } from '$utils/lazy-load-videos';
 
 import { setImageCardAriaLabel } from './components/image-card';
 
-window.Webflow?.push(() => {
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', () => {
+  new LazyLoadVideoEmbeds().init();
+});
+
+window.Webflow = window.Webflow || [];
+window.Webflow.push(() => {
   setImageCardAriaLabel();
   initBugHerd();
 });
