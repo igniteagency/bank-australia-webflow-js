e614d49e07e811fbc6f6158ec0b1db870777526c
Paras Shah <parasshah195@gmail.com>
2025-05-26 19:44:25 +0530

    Update duplicate articles removal to a class; process Finsweet CMS Load articles

--- src/components/blog/duplicates-remove.ts
+++ src/components/blog/duplicates-remove.ts
@@ -1,22 +1,77 @@
 const ARTICLE_SLUG_ATTR = 'data-duplicate-check-slug';
+const ARTICLE_PROCESSED_ATTR = 'data-duplicate-checked';
+
+class DuplicateArticlesRemover {
+  private articleSlugSet: Set<string> = new Set();
+
+  constructor() {
+    this.removeDuplicatesFromDOM();
+    this.reassessOnFinsweetCMSLoad();
+  }
+
+  // Removes duplicates from the DOM using the ARTICLE_SLUG_ATTR
+  removeDuplicatesFromDOM() {
+    const articles = Array.from(
+      document.querySelectorAll(`[${ARTICLE_SLUG_ATTR}]:not([${ARTICLE_PROCESSED_ATTR}])`)
+    );
+    articles.forEach((article) => {
+      this.removeIfDuplicate(article);
+    });
+  }
+
+  reassessOnFinsweetCMSLoad() {
+    (window as any).FinsweetAttributes ||= [];
+    (window as any).FinsweetAttributes.push([
+      'list',
+      (listInstances: any[]) => {
+        const [listInstance] = listInstances;
+        if (!listInstances.length) return;
+
+        listInstance.addHook('beforeRender', (items: any) => {
+          items.forEach((item: any) => {
+            if (item.element?.hasAttribute(ARTICLE_PROCESSED_ATTR)) {
+              return;
+            }
+            const isDuplicate = this.removeIfDuplicate(item.element);
+            if (isDuplicate) {
+              // remove the item from the list
+              listInstance.items.value.splice(items.indexOf(item), 1);
+            }
+          });
+        });
+      },
+    ]);
+  }
+
+  /**
+   * @param article - The article element to check for duplicates
+   * @returns true if the article is a duplicate and has been removed, false otherwise
+   */
+  removeIfDuplicate(article: HTMLElement): boolean {
+    if (article.hasAttribute(ARTICLE_PROCESSED_ATTR)) {
+      return false;
+    }
+
+    const articleSlug = article.getAttribute(ARTICLE_SLUG_ATTR);
+    if (!articleSlug || articleSlug === '') {
+      article.setAttribute(ARTICLE_PROCESSED_ATTR, 'true');
+      return false;
+    }
+
+    if (this.articleSlugSet.has(articleSlug)) {
+      if (window.IS_DEBUG_MODE) {
+        console.debug(`Removing duplicate article - %c${articleSlug}`, 'color: #DC198B;');
+      }
+      article.remove();
+      return true;
+    } else {
+      this.articleSlugSet.add(articleSlug);
+      article.setAttribute(ARTICLE_PROCESSED_ATTR, 'true');
+      return false;
+    }
+  }
+}
 
 window.Webflow?.push(() => {
-  initDuplicateArticlesRemoval();
+  new DuplicateArticlesRemover();
 });
-
-export function initDuplicateArticlesRemoval() {
-  const articleSlugList: string[] = [];
-
-  document.querySelectorAll(`[${ARTICLE_SLUG_ATTR}]`).forEach((articleItemEl) => {
-    const articleSlug = articleItemEl.getAttribute(ARTICLE_SLUG_ATTR);
-    if (!articleSlug || '' === articleSlug) {
-      return;
-    }
-
-    if (articleSlugList.includes(articleSlug)) {
-      articleItemEl.remove();
-    } else {
-      articleSlugList.push(articleSlug);
-    }
-  });
-}
