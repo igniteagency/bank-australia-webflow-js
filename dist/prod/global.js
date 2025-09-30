23d577c7d5cadfc52d058afb9d26252f8028902b
Paras Shah <parasshah195@gmail.com>
2025-09-30 18:00:09 +0530

    Build

--- dist/prod/global.js
+++ dist/prod/global.js
@@ -2,6 +2,33 @@ if (window.SCRIPTS_ENV === 'dev') {
   window.loadLocalScript('http://localhost:3000/global.js');
 } else {
   (() => {
+  // src/components/back-to-top-button.ts
+  function backToTopButton() {
+    const componentEl = document.querySelector(".back-to-top_component");
+    const HIDE_CLASSNAME = "hide";
+    if (!componentEl) return;
+    const threshold = parseInt(componentEl.getAttribute("data-threshold") || "700", 700);
+    function scrollToTop() {
+      const prefersReduced = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+      window.scrollTo({ top: 0, behavior: prefersReduced ? "auto" : "smooth" });
+    }
+    ScrollTrigger.create({
+      trigger: document.body,
+      start: `${threshold}px top`,
+      end: "bottom bottom",
+      onToggle: (self) => {
+        componentEl.classList.toggle(HIDE_CLASSNAME, !self.isActive);
+      },
+      onRefresh: (self) => {
+        componentEl.classList.toggle(HIDE_CLASSNAME, !self.isActive);
+      }
+    });
+    componentEl.addEventListener("click", function(e) {
+      e.preventDefault();
+      scrollToTop();
+    });
+  }
+
   // src/components/dialog-modal.ts
   var DialogRouter = class {
     constructor() {
@@ -219,6 +246,7 @@ if (window.SCRIPTS_ENV === 'dev') {
     setImageCardAriaLabel();
     initBugHerd();
     linkExternal();
+    backToTopButton();
   });
   function disableWebflowScroll() {
     jQuery(document).off("click.wf-scroll");
